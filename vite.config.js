b9b9ac42cea3025de6147e43fa9f3196f44d254f
Jeffrey Yao <jeffrey.yao@relevance.ai>
2023-03-10 14:10:22 +1100

    wip: working build w/ css styles

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import solidSvg from "vite-plugin-solid-svg";
 import devtools from "solid-devtools/vite";
+import postcss from "postcss";
+import tailwindcss from "tailwindcss";
+import postcssImport from "postcss-import";
+import postcssUrl from "postcss-url";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
   plugins: [
@@ -11,6 +16,7 @@ export default defineConfig({
       autoname: true,
       locator: true,
     }),
+    cssInjectedByJsPlugin(),
   ],
   server: {
     port: 3000,
@@ -30,6 +36,25 @@ export default defineConfig({
         chunkFileNames: "[name].js",
         assetFileNames: "[name][extname]",
       },
+      plugins: [
+        postcss({
+          plugins: [
+            tailwindcss({
+              content: ["./src/**/*.tsx", "./src/**/*.jsx"],
+            }),
+            postcssImport(),
+            postcssUrl({
+              url: "inline",
+            }),
+          ],
+          extract: false,
+          modules: false,
+          autoModules: false,
+          minimize: true,
+          inject: false,
+          sourceMap: true,
+        }),
+      ],
     },
   },
 });
